a799b850037af04b132869c6e1a859c5c3461ef9
DReigada <daniel.reigada@gmail.com>
2016-03-03 14:02:44 +0000

    The server now prints the transfer time to the screen

--- server/src/main/java/server/SocketServer.java
+++ server/src/main/java/server/SocketServer.java
@@ -1,26 +1,33 @@
-package example;
+package server;
 
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 import myftp.core.FileReceiver;
-
+import myftp.core.FileSender;
+import myftp.core.ReaderSender;
 
 
 public class SocketServer
 {
     public static void main( String[] args ) throws IOException {
         // Check arguments
-        if (args.length != 2) {
+        if (args.length != 3) {
             System.err.println("Argument(s) missing!");
-            System.err.printf("Usage: java %s port filepath%n", SocketServer.class.getName());
+            System.err.printf("Usage: java %s port mode filepath%n", SocketServer.class.getName());
             return;
         }
 
         // Convert port from String to int
         int port = Integer.parseInt(args[0]);
-        String filePath = args[1];
+        int mode = Integer.parseInt(args[1]);
+        String filePath = args[2];
+        
+        if(mode != 0 && mode != 1){
+        	System.err.printf("Please insert a valid mode (0-receive, 1-send)");
+        	return;
+        }
 
         // Create server socket
         ServerSocket serverSocket = new ServerSocket(port);
@@ -32,10 +39,21 @@ public class SocketServer
         System.out.printf("Connected to client %s on port %d %n",
             clientSocket.getInetAddress().getHostAddress(), clientSocket.getPort());
 
+        ReaderSender transferer;
+        switch (mode){
+        	case 0: // receive mode
+        		transferer = new FileReceiver(clientSocket, filePath);
+        		break;
+        	case 1: // send mode
+        		transferer = new FileSender(clientSocket, filePath);
+        		break;
+        	default: return;
+        }
         
-        FileReceiver receiver = new FileReceiver(clientSocket, filePath); 
-    	receiver.transfer();
-       
+        System.out.print("Starting to transfer the file... \n");
+        long transferTime = transferer.transfer();
+        System.out.printf("Finished transfering file. Elapsed time: " + transferTime/1000 + "," + transferTime%1000 + "s\n");
+        
         // Close connection to current client
         clientSocket.close();
         System.out.println("Closed connection with client");
