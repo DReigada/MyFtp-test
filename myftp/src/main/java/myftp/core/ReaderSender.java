c0574970e5ad529fcbed11e64fcd22d36ff62e70
DReigada <daniel.reigada@gmail.com>
2016-03-03 14:02:07 +0000

    The transfer method in ReaderSender now returns the transfer time

--- myftp/src/main/java/myftp/core/ReaderSender.java
+++ myftp/src/main/java/myftp/core/ReaderSender.java
@@ -4,7 +4,7 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.IOException;
 
-class ReaderSender {
+public abstract class ReaderSender {
 	
 	private BufferedInputStream _in;
 	private BufferedOutputStream _out;
@@ -14,14 +14,19 @@ class ReaderSender {
 		_out = out;
 	}
 	
-	public void transfer() throws IOException{
+	public long transfer() throws IOException{
 	    byte[] buf = new byte[1024];
 	    int bytesRead;
+	    long startTime = System.currentTimeMillis(); // the time before sending/receiving the data
 	    
 	  	while((bytesRead = _in.read(buf)) > 0){
 	    	_out.write(buf, 0, bytesRead);
 	    }
 	  	_out.flush();
+	  	
+	  	long endTime = System.currentTimeMillis(); // the time after sending/receiving the data
+	  	
+	  	return endTime - startTime;
 	}
 	
 }
